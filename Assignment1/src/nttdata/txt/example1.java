e3162148eee028a3e7cc96e19bdd1d235f7c8ce3
Naveena <naveena.bedudoori@gmail.com>
2020-03-26 16:39:43 +0530

    Update example1.java

--- Assignment1/src/nttdata/txt/example1.java
+++ Assignment1/src/nttdata/txt/example1.java
@@ -1,5 +1,20 @@
 package nttdata.txt;
 
+
+import java.util.Scanner;
+
+
+
 public class example1 {
+  
+  
+  public static void main(String [] args)
+	{
+		System.out.println("Welcome Naveena");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Something \t");
+		String p=sc.nextLine();
+	}
+
 
 }
